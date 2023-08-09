e80ab361c1eb18a53eed8922f386cd97740f4397
y-du <42994541+y-du@users.noreply.github.com>
2023-08-09 08:27:42 +0200

    refactor and restructure

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -1,67 +1,6 @@
 #!/bin/sh
 
-plf_linux="linux"
-plf_darwin="darwin"
-
-arch_386="386"
-arch_amd64="amd64"
-arch_arm="arm"
-arch_arm64="arm64"
-
-getPlatform() {
-  if ! plf="$(uname -s)"
-  then
-      exit 1
-  fi
-  case "$plf" in
-    Linux)
-      echo $plf_linux
-      ;;
-#    Darwin)
-#      echo $plf_darwin
-#      ;;
-    *)
-      echo "platform not supported"
-      exit 1
-  esac
-}
-
-getArch() {
-  if ! plf="$(uname -m)"
-  then
-    exit 1
-  fi
-  case "$plf" in
-    x86_64)
-      echo $arch_amd64
-      ;;
-    i386)
-      echo $arch_386
-      ;;
-    aarch64)
-      echo $arch_arm64
-      ;;
-    armv8l)
-      echo $arch_arm64
-      ;;
-    armv7l)
-      echo $arch_arm
-      ;;
-    *)
-      echo "architecture not supported"
-      exit 1
-  esac
-}
-
-checkRoot() {
-  if ! [ "$(id -u)" = "0" ]
-  then
-     echo "root privileges required"
-     exit 1
-  fi
-}
-
-download() {
+downloadFile() {
   path="$2/$(basename "$1")"
   if ! curl -L -o "$path" "$1"
   then
---
+++ scripts/package.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+isAvailable() {
+    command -v "$1" > /dev/null
+    return $?
+}
+
+getMissingCmd() {
+  missing=""
+  for cmd in ${1}
+  do
+    if ! isAvailable "$cmd"
+    then
+      if [ "$missing" = "" ]; then
+        missing="${missing}$cmd"
+      else
+        missing="${missing} $cmd"
+      fi
+    fi
+  done
+  echo "$missing"
+}
+
+installPkg() {
+  if ! apt-get update
+  then
+    exit 1
+  fi
+  if ! apt-get install -y "$1"
+  then
+    exit 1
+  fi
+}
